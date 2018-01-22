2031a08f9d24b2a1f13af67792010160b7ad884a
Chojecka Anna, ENT-CDC-CLU-CDY-APC <Anna.Chojecka@swisscom.com>
2018-01-22 19:51:52 +0100

    add eventListener to cellNode

--- designs.js
+++ designs.js
@@ -4,9 +4,10 @@ let color = document.getElementById('colorPicker').value;
 // Select size input
 let size = document.getElementById('sizePicker');
 
+// select table
 let table = document.getElementById('pixel_canvas');
 
-// make a eveny listener or onClick() on the submit button and (re)assign values for width and height
+// make an event listener or onClick() on the submit button and (re)assign values for width and height
 // add event listener for a <td> in the loop
 // add function to change the backgroundColor
 
@@ -16,9 +17,9 @@ function makeGrid(gridHeight, gridWidth) {
         let row = table.appendChild(rowNode);
         for (var x = 0; x < gridWidth; x++) {
             let cellNode = document.createElement('td');
-            let cell = row.appendChild(cellNode);
-            cell.addEventListener('click', function(color) {
-                cell.style.backgroundColor = color;
+            row.appendChild(cellNode);
+            cellNode.addEventListener('click', function(color) {
+                cellNode.style.backgroundColor = color;
             });
         }
     }
